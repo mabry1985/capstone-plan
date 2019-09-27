9bbd727a072899eba46f7f3ef17922c911651e36
Josh Mabry <mabry1985@gmail.com>
2019-09-27 14:54:56 -0700

    add background image to landing page and more styling

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import Model from './Model';
 import logo from '../assets/images/Logo Small.png';
+import bg from '../assets/images/landing-page-bg.png';
+import tempRoom from '../assets/images/landing-page-bg-room.png'
 import './LandingPage.css'
 
 class LandingPage extends React.Component {
     render() {
     return (
-        <div className='landing-page'>
-            <div className="landing-page-btns">
-                <button className="btn-large login">Login</button> 
-                <br />
-                <button className="btn-large new-account">New Account</button>
+        <main>
+            <img 
+              className= "landing-bg-layer" 
+              src={bg} 
+              alt="background layer" />
+            <div className='landing-page'>
+                <div className="bg">
+                </div>
+                <div className="landing-page-btns">
+                    <button className="btn-large log-in">Log In</button> 
+                    <br />
+                    <button className="btn-large sign-up">Sign Up</button>
+                </div>
+                <div className="logo-landing-page">
+                    <img src={logo} alt="logo"/>
+                    {/* <img src={logo} alt="logo shadow" className="logo-landing-page-shadow"/> */}
+                </div>
+                <img className="temp-room" src={tempRoom} alt=""/>
+                <Model/>
             </div>
-            <div className="logo-landing-page">
-                <img src={logo} alt="logo"/>
-                {/* <img src={logo} alt="logo shadow" className="logo-landing-page-shadow"/> */}
-            </div>
-            <Model/>
-        </div>
+        </main>
     )}
 }
 
