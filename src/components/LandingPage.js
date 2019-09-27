b3aef00e611bd31e14603068aa5f9f36a123f8d7
Josh Mabry <mabry1985@gmail.com>
2019-09-27 13:44:05 -0700

    remove materialize and begin styling landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Model from './Model';
+import logo from '../assets/images/Logo Small.png';
+import './LandingPage.css'
 
 class LandingPage extends React.Component {
     render() {
     return (
         <div className='landing-page'>
-            <button>Login</button>
-            <button>New Account</button>
-            <div className="logo">
-                <img src="#" alt="logo"/>
+            <div className="landing-page-btns">
+                <button className="btn-large login">Login</button> 
+                <br />
+                <button className="btn-large new-account">New Account</button>
+            </div>
+            <div className="logo-landing-page">
+                <img src={logo} alt="logo"/>
+                {/* <img src={logo} alt="logo shadow" className="logo-landing-page-shadow"/> */}
             </div>
             <Model/>
         </div>
