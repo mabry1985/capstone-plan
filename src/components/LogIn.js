0a1ce6e262614af81970c9c2e04421742a137a25
Josh Mabry <mabry1985@gmail.com>
2019-09-28 13:26:56 -0700

    style and position form for log in page

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -9,23 +9,21 @@ const LogIn = () => {
       <Link to="/">
       <img className="logo-log-in" src={logo} alt="logo" />
       </Link>
-      <form onSubmit={"#"} className="white">
-        <h5 className="grey-text text-darken-3">Sign In</h5>
-        <div className="input-field">
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" onChange={"#"} />
-        </div>
-        <div className="input-field">
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" onChange={"#"} />
-        </div>
-        <div className="input-field">
-          <button className="btn z-depth-0">Log In</button>
-          <div className="center">
-            {/* {authError ? <p>{authError}</p> : null} */}
+        <form onSubmit={"#"} className="log-in-form">
+          <div className="input-div">
+              <div className="email-input">
+                <label htmlFor="email" className="a11y">Email</label>
+                <input type="email" id="email" onChange={"#"} placeholder="Email" />
+              </div>
+              <div className="password-input">
+                <label htmlFor="password" className="a11y">Password</label>
+                <input type="password" id="password" onChange={"#"} placeholder="Password" />
+              </div>
           </div>
-        </div>
-      </form>
+          <div className="log-in-but">
+            <button className="log-in-button">Log In</button>
+          </div>
+        </form>
     </div>
   );
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -9,23 +9,22 @@ const SignUp = () => {
       <Link to="/">
         <img className="logo-sign-up" src={logo} alt="logo" />
       </Link>
-      <h5 className="sign-up-h5">Sign Up</h5>
         <form onSubmit={"this.handleSubmit"} className="sign-up-form">
           <div className="input-field">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" onChange={"this.handleChange"} />
+            <label htmlFor="email" className="a11y">Email</label>
+            <input type="email" id="email" onChange={"this.handleChange"} placeholder="Email"/>
           </div>
           <div className="input-field">
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" onChange={"this.handleChange"} />
+            <label htmlFor="password" className="a11y">Password</label>
+            <input type="password" id="password" onChange={"this.handleChange"} placeholder="Password" />
           </div>
           <div className="input-field">
-            <label htmlFor="firstName">First Name</label>
-            <input type="text" id="firstName" onChange={"this.handleChange"} />
+            <label htmlFor="firstName" className="a11y">First Name</label>
+            <input type="text" id="firstName" onChange={"this.handleChange"} placeholder="First Name" />
           </div>
           <div className="input-field">
-            <label htmlFor="lastName">Last Name</label>
-            <input type="text" id="lastName" onChange={"this.handleChange"} />
+            <label htmlFor="lastName" className="a11y">Last Name</label>
+            <input type="text" id="lastName" onChange={"this.handleChange"} placeholder="Last Name" />
           </div>
           <div className="sign-up-button-div">
             <button className="sign-up-button">Sign Up</button>
