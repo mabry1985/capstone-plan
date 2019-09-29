eff0622eb3afc016a121359606ff4ae44bcad25c
Josh Mabry <mabry1985@gmail.com>
2019-09-29 11:01:53 -0700

    add static profile button and box-shadow to menu buttons

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react'
+import './profile.css'
 
 export class Profile extends Component {
+  
+  styles = {
+  }
+
   render() {
     return (
       <div className="profile">
-        <div className="profile-button">
-          <p>JM</p>
-        </div>
-        <div className="options">
-          <ul>
-            <li className="name">Josh Mabry</li>
-            <li className="balance">Balance: $100.00</li>
-            <li className="add-funds">Add Funds</li>
-            <li className="logout">Logout</li>
-            <li className="profile">Profile</li>
+        {/* <div className="table">
+          <ul id="list">
+            <li className="name">Josh Mabry:</li>
+            <li className="balance">$100.00 </li>
+            <li className="add-funds">Add Funds </li>
+            <li> | </li>
+            <li className="profile">Profile </li>
+            <li> | </li>
+            <li className="logout">Logout </li>
           </ul>
+        </div>
+        <div className="profile-button"> */}
+        {/* </div> */}
+        <div className="profile-button">
+          <h4 style={this.styles} className="initials">JM</h4>
         </div>
       </div>
     )
