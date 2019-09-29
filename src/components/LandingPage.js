c4979b827d8f860d019a57878e09d68a83d74279
Josh Mabry <mabry1985@gmail.com>
2019-09-29 14:58:07 -0700

    add state to landing page and build on click listener for login

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,12 +2,28 @@ import React from 'react';
 import Model from './Model';
 import logo from '../assets/images/logo-small.png';
 import bg from '../assets/images/landing-page-bg.png';
-import tempRoom from '../assets/images/landing-page-bg-room.png'
-import './landing-page.css'
+import tempRoom from '../assets/images/landing-page-bg-room.png';
+import './landing-page.css';
 import { Link } from 'react-router-dom';
 
 class LandingPage extends React.Component {
-    render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      logIn: false,
+      signIn: false,
+    }
+  }
+
+  handleClickLogIn = (state) => {
+    this.setState({
+      logIn: true,
+    })
+    console.log(this.state)
+  }
+
+  render() {
     return (
         <main>
             <img 
@@ -16,21 +32,24 @@ class LandingPage extends React.Component {
               alt="background layer" />
             <div className='landing-page'>
                 <div className="landing-page-btns">
-                    <Link to="/log-in">
-                      <button className="log-in">Log In</button> 
-                    </Link>
+                      <button 
+                        className="log-in"
+                        onClick={this.handleClickLogIn}>
+                        Log In
+                      </button> 
                     <br />
                     <Link to="/sign-up">
                       <button className="sign-up">Sign Up</button>
                     </Link>
                 </div>
-                 <img className="logo-landing-page" src={logo} alt="logo"/>
+                  <img className="logo-landing-page" src={logo} alt="logo"/>
                   {/* <img src={logo} alt="logo shadow" /> */}
                 <img className="temp-room" src={tempRoom} alt=""/>
                 <Model/>
             </div>
         </main>
-    )}
+    )
+  }
 }
 
 export default LandingPage
