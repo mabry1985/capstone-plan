db736b6346f4729f1362e75500bf1c95621cde14
Josh Mabry <mabry1985@gmail.com>
2019-09-29 09:09:00 -0700

    align buttons and logo on main menu, begin work on profile header

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -5,40 +5,39 @@ import menuButton from '../assets/images/menu-button.png';
 import sellButton from '../assets/images/sell-button.png';
 import statsButton from '../assets/images/stats-button.png';
 import chatButton from '../assets/images/chat-button.png';
+import gameButton from '../assets/images/games-button.png';
 import logo from '../assets/images/logo-small.png';
+import './main-menu.css';
 
 const MainMenu = () => {
   return (
-    <div className= "main-menu-div">
-      <div className="main-menu-buttons menu-button">
-        <Link to='/menu'>
-          <img src={menuButton} alt="menu button"/> 
-        </Link>
+    <div className="main-menu-page">
+      <Profile/>
+      <div className= "main-menu-buttons-div">
+          <Link to='/menu'>
+            <img className="main-menu-buttons sell-button" src={menuButton} alt="menu button"/> 
+          </Link>
+
+          <Link to='/sell'>
+            <img className="main-menu-buttons sell-button" src={sellButton} alt="sell button" /> 
+          </Link>
+        
+          <Link to='/stats'>
+            <img className="main-menu-buttons sell-button" src={statsButton} alt="stats button" />
+          </Link>
+        
+          <Link to='/chat'>
+            <img className="main-menu-buttons sell-button" src={chatButton} alt="chat button" />
+          </Link>
+
+          <Link to='/games'>
+            <img className="main-menu-buttons game-button" src={gameButton} alt="game button" />
+          </Link>
       </div>
-      <div className="main-menu-buttons sell-button">
-        <Link to='/sell'>
-          <img src={sellButton} alt="sell button" /> 
-        </Link>
-      </div>
-      
-      <div className="main-menu-buttons stats-button">
-        <Link to='/stats'>
-          <img src={statsButton} alt="stats button" />
-        </Link>
-      </div>
-      
-      <div className="main-menu-buttons chat-button">
-        <Link to='/chat'>
-          <img src={chatButton} alt="chat button" />
-        </Link>
-      </div>
-      
-      <div className="main-menu-buttons games-button">
-        <Link to='/games'>Games</Link>
-      </div>
-      <div className="logo">
-        <img src={logo} alt="logo" />
-      </div>
+          <Link to='/'>
+            <img className="logo-main-menu" src={logo} alt="logo" />
+          </Link>
+
     </div>
   )
 }
