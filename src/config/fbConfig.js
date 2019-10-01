6cba67e13a9e48ea86ae25aae2ec19d39d5ef57c
Josh Mabry <mabry1985@gmail.com>
2019-10-01 12:31:58 -0700

    add firebase/firestore

---
+++ src/config/fbConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyAqMnZ-XSOdFp5vz_IFXqpnYbGRJDvQd48",
+  authDomain: "hole-in-the-wall-57859.firebaseapp.com",
+  databaseURL: "https://hole-in-the-wall-57859.firebaseio.com",
+  projectId: "hole-in-the-wall-57859",
+  storageBucket: "hole-in-the-wall-57859.appspot.com",
+  messagingSenderId: "444610123075",
+  appId: "1:444610123075:web:89bca22c4dd688d6744bf6"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true })
+
+export default firebase;
